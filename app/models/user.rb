af85652ce7ee64b196c59cc29e0ffe5c01106b65
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-08 13:04:35 +0300

    commit tree bc1f38db6e86c2763f293569c1ca67030b7b934f

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+# class User < ApplicationRecord
+#     has_many :recipes
+
+#     has_secure_passwo:redirect
+
+#     validates :username, presence: true, uniqueness: true
+# end
---
+++ db/migrate/20221008095234_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.belongs_to :user
+      t.string :title
+      t.text :instructions
+      t.integer :minute_to_complete
+      t.timestamps
+    end
+  end
+end
